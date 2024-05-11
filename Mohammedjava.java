ccea649c8230d2d4eb16a6618cd3dc14ab416c5f
unknown <cueit23004@cue.edu.krd>
2024-05-11 16:21:43 +0300

    one comment added to java program

---
+++ Mohammedjava.java
@@ -0,0 +1,6 @@
+public class Mohammedjava {
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("hello World! after the edit");
+    }
+   }
